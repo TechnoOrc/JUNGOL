3e209e9c08655a65c18c163a27a746da1cfd4b44
TechnoOrc <technoorc@gmail.com>
2022-06-23 09:32:51 +0900

    2022.06.23

---
+++ JUNGOL/src/array/Array562.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Scanner;
+
+public class Array562 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int iArr [] = new int [10];
+		int sum1 = 0;
+		int sum2 = 0;
+		int count = 0;
+		
+		for (int i = 0; i < iArr.length; i++) {
+			iArr[i] = scan.nextInt();
+			if ( i % 2 == 1) {
+				sum1 = sum1 + iArr[i];
+			} else {
+				sum2 = sum2 + iArr[i];
+				count++;
+			}//if
+		}//for
+		double avg = (double) sum2 / count;
+		System.out.println("sum : " + sum1);
+		System.out.printf("avg : " + "%.1f", avg);
+		
+	}//main
+
+}//class
--- JUNGOL/src/input/Input512.java
+++ JUNGOL/src/input/Input512.java
@@ -2,9 +2,10 @@ package input;
 
 public class Input512 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+	public static void main(String [] args) {
+		
+		int iVar1 = 49;
+		double dVar1 = 0.268300d;
+		System.out.printf("%d * %f = %f",iVar1,dVar1,iVar1*dVar1);
+	}//main
+}//class
