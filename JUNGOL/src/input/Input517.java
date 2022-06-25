e45c18de2d556c78e8121b57de1751cd8f8b72bc
TechnoOrc <technoorc@gmail.com>
2022-06-25 23:56:51 +0900

    정올_517:입력 자가진단9

---
+++ JUNGOL/src/input/Input517.java
@@ -0,0 +1,19 @@
+package input;
+
+import java.util.Scanner;
+
+public class Input517 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		double dVar1 = 0;
+		double dVar2 = 0;
+		double dVar3 = 0;
+		
+		dVar1 = scan.nextDouble();
+		dVar2 = scan.nextDouble();
+		dVar3 = scan.nextDouble();
+	}
+
+}
