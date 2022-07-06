179d1045e626f64b5a747030e959b82c6862b014
TechnoOrc <technoorc@gmail.com>
2022-07-06 23:47:43 +0900

    2022.07.06 정올 문제풀이

--- JUNGOL/src/selection/Selection120.java
+++ JUNGOL/src/selection/Selection120.java
@@ -1,10 +1,37 @@
 package selection;
 
+import java.util.Scanner;
+
+/*
+ * 문제 
+ * 두 개의 정수를 입력받아 큰 수에서 작은 수를 뺀 차를 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력 예
+ * 50 85
+ * 
+ * 출력 예
+ * 35
+ */
+
 public class Selection120 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int iVar1, iVar2;
+		
+		iVar1 = sc.nextInt();
+		iVar2 = sc.nextInt();
+		
+		sc.close();
+		int result = iVar1 - iVar2;
+		
+		if(result < 0)
+		{
+			result = result * -1;
+		}
+		System.out.println(result);
+		
+	}//main
 
-	}
-
-}
+}//class
--- JUNGOL/src/selection/Selection121.java
+++ JUNGOL/src/selection/Selection121.java
@@ -1,10 +1,32 @@
 package selection;
 
+import java.util.Scanner;
+
+/*
+ * 문제
+ * 정수를 입력받아 0 이면 "zero" 양수이면 "plus" 음수이면 "minus" 라고 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력 예
+ * 0
+ * 
+ * 출력 예
+ * zero
+ */
 public class Selection121 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		int iVar = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		iVar = sc.nextInt();
+		if(iVar > 0 ) {
+			System.out.println("plus");
+		} else if(iVar < 0 ) {
+			System.out.println("minus");
+		} else System.out.println("zero");
+		
+		
+	}//main
 
-}
+}//class
